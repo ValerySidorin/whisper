e36b2bd0d5e75d2c34b8bdea96907ca5ebe1c1a8
Valery Sidorin <vs@emsoft.ru>
2022-06-29 19:30:04 +0300

    Refactor message renderer

--- internal/domain/message_renderer.go
+++ internal/domain/message_renderer.go
@@ -1,8 +1,6 @@
 package domain
 
 import (
-	"fmt"
-
 	"github.com/ValerySidorin/whisper/internal/domain/dto"
 )
 
@@ -16,8 +14,7 @@ func RegisterDefaultMessageRenderer() *DefaultMessageRenderer {
 func (r *DefaultMessageRenderer) RenderMergeRequest(mr *dto.MergeRequest) string {
 	switch mr.State {
 	case "opened":
-		return fmt.Sprintf("New Merge Request! | %v\n-- -- -- --\nTitle: %v\nDescription: %v\n%v\n-- -- -- --\nBranch: %v → %v\nAuthor: %v",
-			mr.Project.Name, mr.Title, mr.Description, mr.URL, mr.SourceBranch, mr.TargetBranch, mr.Author.Name)
+		return "New Merge Request! | " + mr.Project.Name + "\n-- -- -- --\nTitle: " + mr.Title + "\nDescription:  + " + mr.Description + "\n" + mr.URL + "\n-- -- -- --\nBranch: " + mr.SourceBranch + "→ " + mr.TargetBranch + "\nAuthor: " + mr.Author.Name
 	default:
 		return ""
 	}
@@ -26,9 +23,9 @@ func (r *DefaultMessageRenderer) RenderMergeRequest(mr *dto.MergeRequest) string
 func (r *DefaultMessageRenderer) RenderDeployment(d *dto.Deployment) string {
 	switch d.Status {
 	case "success":
-		return fmt.Sprintf("Deployment succeded! | %v\n-- -- -- --\nEnv: %v\n-- -- -- --\nJob: %v\n-- -- -- --\nCommit: %v\n%v\n-- -- -- --\nInitiator: %v", d.Project.Name, d.Environment, d.Job.Name, d.CommitTitle, d.CommitURL, d.User.Name)
+		return "Deployment succeded! | " + d.Project.Name + "\n-- -- -- --\nEnv: " + d.Environment + "\n-- -- -- --\nJob: " + d.Job.Name + "\n-- -- -- --\nCommit: " + d.CommitTitle + "\n" + d.CommitURL + "\n-- -- -- --\nInitiator: " + d.User.Name
 	case "failed":
-		return fmt.Sprintf("Deployment failed! | %v\n-- -- -- --\nEnv: %v\n-- -- -- --\n→ %v\n-- -- -- --\nJob: %v\n%v", d.Project.Name, d.Environment, d.Status, d.Job.Name, d.DeployableURL)
+		return "Deployment failed! | " + d.Project.Name + "\n-- -- -- --\nEnv: " + d.Environment + "\n-- -- -- --\n-- -- -- --\nJob: " + d.Job.Name + "\n" + d.DeployableURL
 	default:
 		return ""
 	}
