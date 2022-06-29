253b6c68a137009b830d25354716e674bd5d6886
Valery Sidorin <vs@emsoft.ru>
2022-06-29 14:42:17 +0300

    Update merge request message (temp)

--- internal/domain/dto/mergerequest.go
+++ internal/domain/dto/mergerequest.go
@@ -2,6 +2,8 @@ package dto
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 )
 
 type MergeRequest struct {
@@ -20,13 +22,19 @@ type MergeRequest struct {
 }
 
 func (mr *MergeRequest) GetMessage() string {
+	trackerLink := ""
+	matched, _ := regexp.MatchString(`^\w+-\d+\s`, mr.Title)
+	if matched {
+		trackerLink = "https://tracker.yandex.ru/" + mr.Title[:strings.IndexByte(mr.Title, ' ')]
+	}
+
 	switch mr.State {
 	case "opened":
 		return fmt.Sprintf("New Merge Request! | %v\n-- -- -- --\nTitle: %v\nDescription: %v\n%v\n-- -- -- --\nBranch: %v → %v\nAuthor: %v",
 			mr.Project.Name, mr.Title, mr.Description, mr.URL, mr.SourceBranch, mr.TargetBranch, mr.Author.Name)
 	case "merged":
-		return fmt.Sprintf("Merge Request has been merged! | %v\n-- -- -- --\nTitle: %v\nDescription: %v\n%v\n-- -- -- --\nBranch: %v → %v\nAuthor: %v",
-			mr.Project.Name, mr.Title, mr.Description, mr.URL, mr.SourceBranch, mr.TargetBranch, mr.Author.Name)
+		return fmt.Sprintf("Merge Request has been merged! | %v\n-- -- -- --\nTitle: %v\nDescription: %v\n%v\n-- -- -- --\nTracker: %v\n-- -- -- --\nBranch: %v → %v\nAuthor: %v",
+			mr.Project.Name, mr.Title, mr.Description, mr.URL, trackerLink, mr.SourceBranch, mr.TargetBranch, mr.Author.Name)
 	default:
 		return fmt.Sprintf("Merge Request status changed! | %v\n-- -- -- --\n→ %v", mr.Project.Name, mr.State)
 	}
